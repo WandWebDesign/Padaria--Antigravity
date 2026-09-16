2cad659863b675a308d5c6c834456f7c4fdc0990
WandWebDesign <144054215+WandWebDesign@users.noreply.github.com>
2026-09-16 13:02:24 -0300

    Update agendamento.js

--- Script/agendamento.js
+++ Script/agendamento.js
@@ -147,7 +147,7 @@ btnAdicionar.addEventListener("click", () => {
         nome: produtoAtual.nome,
         preco: precoVigente,
         quantidade: quantidade,
-        imagem: produtoAtual.imagem,
+        imagem: produtoAtual.imagem_url,
         quantidade_estoque_real: produtoAtual.quantidade_estoque, 
         dataRetirada: null,  
         horaRetirada: null   
